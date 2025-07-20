02522d0eb7c381643068626f44d8f54e6ed41671
“Abhishek <abhishekpipriye2507@gmail.com>
2025-07-20 15:24:07 +0530

    refactor: update motion import and simplify App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import {motion} from 'motion/react'
 const App = () => {
-
   return (
-
-    // <div className="scroll-container">
-    //   <motion.div
-    //     className="scroll-text"
-    //     animate={{ x: ["100%", "-100%"] }}
-    //     transition={{
-    //       repeat: Infinity,
-    //       duration: 10,
-    //       ease: "linear",
-    //     }}
-    //   >
-    //     Abhishek Radhakishan Pipariye &nbsp; 🚀&nbsp; Abhishek Radhakishan Pipariye &nbsp; 🚀&nbsp; {/* repeat for infinite feel */}
-    //   </motion.div>
-    // </div>
-
-    <div>
-      <motion.div className="box"
-      animate={{
-        x:[0,700,500,300,200,1000,250],
-        y:[0,0,400,200,110,500,1000]
-      }} 
-      transition={{duration:4,delay:1}} ></motion.div>
-    </div>
+    <motion.div 
+    whileHover={{
+      background:'yellow'
+    }} 
+    whileTap={{
+      scale:0.9
+    }} className='box'
+    >
+    
+      
+    </motion.div>
   )
 }
 
